6d57cc46a5f9cbe10e8f536bcc22f4dca253a955
Antoine <aberthier15@gmail.com>
2020-12-01 13:44:28 +0100

    fix gestion pod

--- command-podman.sh
+++ command-podman.sh
@@ -1,30 +1,15 @@
 #!/bin/sh
 
-#FILE_INFLUX=./influxdb
-#FILE_GRAFANA=./grafana
-
-#if [ ! -d "$FILE_INFLUX" ];then
-#    mkdir  "$FILE_INFLUX"
-#fi   
-
-#if [ ! -d "$FILE_GRAFANA" ];then
-#    mkdir "$FILE_GRAFANA"
-#fi   
 
 podman pod exists appweb
 if [ $? -eq 1 ]
 then
-    podman pod create --name appweb -p 3000:3000 --hostname '0.0.0.0'
-fi
-
-podman pod exists appbd
-if [ $? -eq 1 ]
-then
-    podman pod create --name appbd  -p 8086:8086/udp --hostname '0.0.0.0'
+    podman pod create --name appweb  --hostname '0.0.0.0'
 fi
 
 
 
-podman run -dt --env-file='.grafana.env'  --name=grafana --restart=always  --pod appweb grafana/grafana:latest
 
-podman run -dt --env-file='.influxdb.env'  --name=influxdb --restart=always  -v ./influxdb:/var/lib/influxdb --pod appbd influxdb
+podman run -dt --env-file='.grafana.env'  --name=grafana --restart=always -p 3000:3000  --pod appweb grafana/grafana:latest
+
+podman run -dt --env-file='.influxdb.env'  --name=influxdb --restart=always -p 8086:8086  -v ./influxdb:/var/lib/influxdb --pod appweb influxdb
