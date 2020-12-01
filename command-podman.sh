490cd793c131a85074515e6ba6bb85cb836ae44a
Antoine <aberthier15@gmail.com>
2020-12-01 11:55:40 +0100

    prepare volume

--- command-podman.sh
+++ command-podman.sh
@@ -1,9 +1,21 @@
 #!/bin/sh
 
+FILE_INFLUX=./influxdb
+FILE_GRAFANA=./grafana
+
+if [ ! -f "$FILE_INFLUX" ];then
+    mkdir  "$FILE_INFLUX"
+fi   
+
+if [ ! -f "$FILE_GRAFANA" ];then
+    mkdir "$FILE_GRAFANA"
+fi   
+
 podman pod exists apps
 if [ $? -eq 1 ]
 then
     podman pod create --name apps -p 3000:3000 -p 8086:8086
-fi    
+fi
+
 podman run -dt --env-file='.grafana.env'  --name=grafana --restart=always -v ./grafana:/var/lib/grafana --pod apps grafana/grafana:latest
 podman run -dt --env-file='.influxdb.env'  --name=influxdb --restart=always  -v ./influxdb:/var/lib/influxdb --pod apps influxdb
