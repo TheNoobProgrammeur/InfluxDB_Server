5ed6555f24eefcea4c176aa311eea7e4ad77b3f2
Antoine <aberthier15@gmail.com>
2020-12-01 13:46:33 +0100

    fix port

--- command-podman.sh
+++ command-podman.sh
@@ -4,12 +4,12 @@
 podman pod exists appweb
 if [ $? -eq 1 ]
 then
-    podman pod create --name appweb  --hostname '0.0.0.0'
+    podman pod create --name appweb -p 8086:8086 -p 3000:3000   --hostname '0.0.0.0'
 fi
 
 
 
 
-podman run -dt --env-file='.grafana.env'  --name=grafana --restart=always -p 3000:3000  --pod appweb grafana/grafana:latest
+podman run -dt --env-file='.grafana.env'  --name=grafana --restart=always  --pod appweb grafana/grafana:latest
 
-podman run -dt --env-file='.influxdb.env'  --name=influxdb --restart=always -p 8086:8086  -v ./influxdb:/var/lib/influxdb --pod appweb influxdb
+podman run -dt --env-file='.influxdb.env'  --name=influxdb --restart=always  -v ./influxdb:/var/lib/influxdb --pod appweb influxdb
