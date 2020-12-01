8f9beee6e7db66c58042578089ecf789a61fe65f
Antoine <aberthier15@gmail.com>
2020-12-01 11:27:34 +0100

    fix env file format

--- command-podman.sh
+++ command-podman.sh
@@ -5,5 +5,5 @@ if [ $0 -eq 1 ];
 then
     podman pod create --name apps -p 3000:3000 -p 8086:8086
 fi    
-podman run -dt –env-file='env.grafana'  –name=grafana –restart=always -v ./grafana:/var/lib/grafana --pod apps grafana/grafana:latest
-podman run -dt –env-file='env.influxdb'  –name=influxdb –restart=always  -v ./influxdb:/var/lib/influxdb --pod apps influxdb
+podman run -dt –env-file='.grafana.env'  –name=grafana –restart=always -v ./grafana:/var/lib/grafana --pod apps grafana/grafana:latest
+podman run -dt –env-file='.influxdb.env'  –name=influxdb –restart=always  -v ./influxdb:/var/lib/influxdb --pod apps influxdb
